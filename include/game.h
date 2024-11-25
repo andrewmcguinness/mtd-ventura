c438c27792a08936df02585aeb99e2e50a70760b
andrew <andrew@lfawhsq2.arobeia.co.uk>
2024-11-25 13:54:30 +0000

    more tests

--- include/game.h
+++ include/game.h
@@ -2,11 +2,21 @@
 
 #include <vector>
 
+struct tile_pos {
+  bool m1;
+  bool m2;
+  operator bool() const { return m1 || m2; }
+};
+
 struct tile {
   tile(int v1, int v2) : val((v1<<4) + v2) {}
   int v1() const { return ((unsigned)val) >> 4; }
   int v2() const { return val & 0x0f; }
   int v(int n) const { return (n==1)?v2():v1(); }
+  bool dub() const { return v1() == v2(); }
+  int score() const { return v1() + v2() + (val?0:30); }
+  tile_pos has(int n) const { return { v1() == n, v2() == n }; } 
+  int other(const tile_pos& tp) const { return tp.m1?(v2()):v1(); }
   bool operator == (const tile& other) const { return val == other.val; }
   bool operator < (const tile& other) const { return val < other.val; }
   bool operator > (const tile& other) const { return val > other.val; }
@@ -22,6 +32,7 @@ struct pool {
 
 struct track {
   track(int s) : start(s), end(s), train_on(false) {}
+  bool add(tile t);
 
   int start;
   int end;
